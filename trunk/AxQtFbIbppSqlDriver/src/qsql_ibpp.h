03ccbc87b2eb809ec7667476155f1a782a00e7f9
alex.wencel <alex.wencel@d2e2fb96-f781-11dd-ac65-f74f249cfaac>
2010-02-15 15:51:08 +0000

    - fixed Produce different targets depengind on config (Debug Release) (Issue 6) + license under LGPL version 2.1 + license under GPL version 3.0

--- trunk/AxQtFbIbppSqlDriver/src/qsql_ibpp.h
+++ trunk/AxQtFbIbppSqlDriver/src/qsql_ibpp.h
@@ -2,23 +2,24 @@
 * This file is part of QtFirebirdIBPPSQLDriver - Qt SQL driver for Firebird with IBPP library
 * Copyright (C) 2006-2010 Alex Wencel
 *
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*
 * Contact e-mail: Alex Wencel <alex.wencel@gmail.com>
 * Program URL   : http://code.google.com/p/qtfirebirdibppsqldriver
+*
+* GNU Lesser General Public License Usage
+* This file may be used under the terms of the GNU Lesser
+* General Public License version 2.1 as published by the Free Software
+* Foundation and appearing in the file LICENSE.LGPL included in the
+* packaging of this file.  Please review the following information to
+* ensure the GNU Lesser General Public License version 2.1 requirements
+* will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+*
+* GNU General Public License Usage
+* Alternatively, this file may be used under the terms of the GNU
+* General Public License version 3.0 as published by the Free Software
+* Foundation and appearing in the file LICENSE.GPL included in the
+* packaging of this file.  Please review the following information to
+* ensure the GNU General Public License version 3.0 requirements will be
+* met: http://www.gnu.org/copyleft/gpl.html.
 *
 */
 
